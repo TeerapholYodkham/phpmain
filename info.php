b7d4632264c64d4d2a1e992b956ba17e8b0ae42b
TeerapholYodkham <123447348+TeerapholYodkham@users.noreply.github.com>
2023-03-02 19:51:30 +0700

    Update info.php

---
+++ info.php
@@ -0,0 +1,8 @@
+<?php 
+  $string1 = 1;
+  $string2 = 1;
+  
+  $display = (isset($string1) && isset($string2) && $string1 == 1 && $string2 == 1) ? "display true" : "display false";
+  
+  echo $display;
+?>
